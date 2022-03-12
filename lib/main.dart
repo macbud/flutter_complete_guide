138d4f214de83ec67d11ef41de1f60b2ae90828a
macbud <77940567+macbud@users.noreply.github.com>
2022-03-12 19:31:03 +0100

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-// ignore_for_file: deprecated_member_use
-
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  MyApp({Key? key}) : super(key: key);
   var questionIndex = 0;
 
   void answerQuestion() {
